6fc5890e60be927f8fcbf577320c7a82fa2d5634
Methode <methode1d@gmail.com>
2025-07-02 18:48:22 +0200

    All requirements added

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./mydatabase.db'
+db = SQLAlchemy(app)
+print("Database URI resolved to:", app.config['SQLALCHEMY_DATABASE_URI'])
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    Date_Created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task: {self.content}>"
+
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/home')
+def home_page():
+
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new = ToDo(content=task_content)
+        db.session.add(new)
+        db.session.commit()
+        return redirect('/home')
+    else:
+        tasks = ToDo.query.order_by(ToDo.Date_Created).all()
+        return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    ttodel = ToDo.query.get_or_404(id)
+    db.session.delete(ttodel)
+    db.session.commit()
+
+    return redirect('/')
+
+@app.route('/update/<int:id>')
+def update(id):
+    ttoup = ToDo.query.get_or_404(id)
+    old_delete = ttoup
+    db.session.delete(old_delete)
+    db.session.commit()
+    return render_template('updated.html', task=ttoup)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        print("The current working directory is:", os.getcwd())
+        db.create_all()
+        print("Database is succesfully created in current directory")
+
+    app.run(debug=True)
