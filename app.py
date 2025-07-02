e93e7d2c3416f9ce392f51081fc55f2e94df3322
Methode <methode1d@gmail.com>
2025-07-02 23:30:04 +0200

    updating app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ if __name__ == "__main__":
         db.create_all()
         print("Database is succesfully created in current directory")
 
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
